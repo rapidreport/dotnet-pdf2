d1f7fc52d6d36bd09275bf3dc682f286bf994a46
Shuetsu Ito <shuetsu@systembase.co.jp>
2016-04-15 19:01:45 +0900

    PDF出力時にline_width=0の場合でもrect/circleの枠線が表示されてしまう場合があった問題を修正

--- test/Main.vb
+++ test/Main.vb
@@ -10,6 +10,7 @@
         Tests.Add(New Test_0_2)
         Tests.Add(New Test_0_3)
         Tests.Add(New Test_0_5_PDF)
+        Tests.Add(New Test_4_23_Rect)
 
         Application.Run(New FmTest)
     End Sub
